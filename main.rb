4c7f68bee9079696fb27dbbf64b1a2ae69eb1d9b
yajitaku <phenomenon.ar@gmail.com>
2015-04-04 11:52:53 +0900

    fix ~p112

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+# encoding: utf-8
 require 'sinatra'
 require 'sqlite3'
-require 'pp'
+require 'securerandom'
 
-db = SQLite3::Database.new "db/post.db"
+db =　SQLite3::Database.new "db/post.db"
 db.results_as_hash = true
 
 get '/' do
@@ -12,8 +13,31 @@ get '/' do
 end
 
 post '/' do
-  stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
-  stmt.bind_params(params["ex_text"])
+  file_name = ""
+
+  if params["file"]
+    ext = ""
+    if params["file"][:type].include? "jpeg"
+      ext = "jpg"
+    elsif params["file"][:type].include? "png"
+      ext = "png"
+    else
+      return "投稿出来る画像形式はjpgとpngだけです"
+    end
+
+    #適当なファイル名をつける
+    file_name = SecureRandom.hex + "." + ext
+
+    #画像を保存
+    File.open("./public/uploads/" + file_name, 'wb') do |f|
+      f.write paramas["file"][:tempfile].read
+    end
+  else
+    return "画像が必須です"
+  end
+  
+  stmt = db.prepare("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
+  stmt.bind_params(params["ex_text"], file_name)
   stmt.execute
   redirect '/'
 end
