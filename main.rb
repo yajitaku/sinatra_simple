92cc2f7201621bab1244dd0fad0bbbbbcc62be6a
yajitaku <phenomenon.ar@gmail.com>
2015-04-04 11:17:04 +0900

    fix

--- main.rb
+++ main.rb
@@ -1,7 +1,21 @@
+# -*- coding: utf-8 -*-
 require 'sinatra'
+require 'sqlite3'
+require 'pp'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
 
 get '/' do
-  erb :index
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+  erb :index, { :locals => { :posts => posts } }
+end
+
+post '/' do
+  stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
+  stmt.bind_params(params["ex_text"])
+  stmt.execute
+  redirect '/'
 end
 
 get '/hello' do
