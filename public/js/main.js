93f340cadd9862e54a27fdb76f19875fc9eb398b
yajitaku <phenomenon.ar@gmail.com>
2015-04-09 00:02:22 +0900

    last fix

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+
+
+$(function(){
+    $('.add-star').click(function(){
+	var $this = $(this);
+	var post_id = $this.attr('data-post-id');
+	$.ajax({
+	    url: '/star?post_id=' + post_id,
+	    datatype: 'json',
+	    success: function(data) {
+		$this.text(data['star_count']);
+	    },
+	    error: function(data) {
+		alert('失敗しました');
+	    }
+	});	
+    });
+});
--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+# encoding: utf-8
 require 'sinatra'
 require 'sqlite3'
 require 'securerandom'
 require 'sinatra/json'
 
-db = SQLite3::Database.new "db/post.db"
+db =　SQLite3::Database.new "db/post.db"
 db.results_as_hash = true
 
 get '/' do
@@ -22,39 +23,24 @@ post '/' do
     elsif params["file"][:type].include? "png"
       ext = "png"
     else
-      return "投稿できる画像形式はjpgとpngだけです"
+      return "投稿出来る画像形式はjpgとpngだけです"
     end
 
-    # 適当なファイル名を付ける
+    #適当なファイル名をつける
     file_name = SecureRandom.hex + "." + ext
 
-    # 画像を保存
+    #画像を保存
     File.open("./public/uploads/" + file_name, 'wb') do |f|
-      f.write params["file"][:tempfile].read
+      f.write paramas["file"][:tempfile].read
     end
   else
     return "画像が必須です"
   end
-
+  
   stmt = db.prepare("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
   stmt.bind_params(params["ex_text"], file_name)
   stmt.execute
   redirect '/'
-end
-
-get '/star' do
-  post_id = params["post_id"].to_i
-  post = db.execute("SELECT star_count FROM posts WHERE id = ?", post_id)
-  if post.empty?
-    return "error"
-  end
-  new_star_count = post[0]["star_count"] + 1
-  stmt = db.prepare("UPDATE posts SET star_count = ? WHERE id = ?")
-  stmt.bind_params(new_star_count, post_id)
-  stmt.execute
-
-  response = { "star_count" => new_star_count }
-  json response
 end
 
 get '/hello' do
@@ -63,4 +49,20 @@ end
 
 get '/example' do
   erb :example
+end
+
+get '/star' do
+  post_id = params["post_id"].to_i
+  post = db.execute("SELECT star_count FROM posts WHERE id = ?", post_id)
+  if post.empty?
+    return "error"
+  end
+  
+  new_star_count = post[0]["star_count"] + 1
+  stmt = db.prepare("UPDATE posts SET star_count = ? WHERE id = ?")
+  stmt.bind_params(new_star_count, post_id)
+  stmt.execute
+  
+  response = { "star_count" => new_star_count }
+  json responsex
 end
